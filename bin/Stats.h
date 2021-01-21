10b559eadb8dcccca8099ce6069129beccbd7411
bouhnome <62355679+bouhnome@users.noreply.github.com>
2021-01-21 23:49:48 +0100

    Add files via upload

---
+++ bin/Stats.h
@@ -0,0 +1,112 @@
+/*************************************************************************
+                           Stats  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Interface de la classe <Stats> (fichier Stats.h) ----------------
+#if ! defined ( Stats_H )
+#define Stats_H
+
+//--------------------------------------------------- Interfaces utilisées
+#include "FluxLog.h"
+#include <map>
+#include <unordered_map>
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+typedef unordered_map<string,unordered_map<string,unsigned int> > MapPrincipal;
+typedef unordered_map<string,unsigned int> MapSecondaire1;
+typedef multimap<unsigned int, string> MapSecondaire2;
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Stats>
+//
+//
+//------------------------------------------------------------------------
+
+class Stats 
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste des paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+
+//------------------------------------------------- Surcharge d'opérateurs
+    /*Stats & operator = ( const Stats & unStats );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+    */
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    Stats ( const Stats & unStats );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+    Stats ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~Stats ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    void AjouterGraphe(string Cible, string Referent);
+    // Mode d'emploi :
+    // Ajouter une ligne dans la MapGraphe
+    // Contrat :
+    //
+
+    void AjouterTop10(string Cible);
+    // Mode d'emploi :
+    // Ajouter une ligne dans la MapTop10
+    // Contrat :
+    //
+	
+    void GenererGraphe();
+    // Mode d'emploi :
+    // Genere le fichier graphe nomFichier.dot
+    // Contrat :
+    //
+
+    void Top10();
+    // Mode d'emploi :
+    // Génère les attributs Top10 et affiche le résultat
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+    MapPrincipal MapGraphe;
+    MapSecondaire1 MapTop10_1;
+    MapSecondaire2 MapTop10_2;
+    
+
+
+};
+
+//-------------------------------- Autres définitions dépendantes de <Stats>
+
+#endif // Stats_H
+
---
+++ bin/installation.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp analog /usr/local/bin/
+cp analog.1.gz /usr/local/share/man/man1/
