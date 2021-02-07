2ed3ecfcbb195fcb188fef6e78c351abf5bdc065
bouhnome <62355679+bouhnome@users.noreply.github.com>
2021-02-07 19:33:45 +0100

    Add files via upload

--- bin/toolmain.h
+++ bin/toolmain.h
@@ -5,7 +5,10 @@ using namespace std;
 #include <string.h>
 #include "toolmain1.h"
 void onlyLogFile(const char * logFileNamec);
+void logFile_e(const char * logFileNamec);
 void logFile_g(const char * logFileNamec, const char * fileNameDotc);
+void logFile_t(const char * logFileNamec, const char * Heurec);
+void logFile_t_e(const char * logFileNamec, const char * Heurec);
 void logFile_g_t(const char * logFileNamec,const char * fileNameDotc,const char * Heurec);
 void logFile_g_e(const char * logFileNamec, const char * fileNameDotc);
 void logFile_g_t_e(const char * logFileNamec,const char * fileNameDotc,const char * Heurec);
